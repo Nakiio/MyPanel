1339c26d932b7f59ff0a4993e8e15108cf0ab8f2
Nakiio <86538281+Nakiio@users.noreply.github.com>
2022-12-04 22:54:14 +0100

    Add files via upload

---
+++ Js/clickIcon.js
@@ -0,0 +1,7 @@
+window.onclick = e => {
+  //if (window.confirm("Do you want run : " + e.target.id)){
+    console.log(e.target.id)
+  //}else{
+  }
+//} 
+
---
+++ Js/init.js
@@ -0,0 +1,2 @@
+//document.getElementById("username").innerHTML = "Name : Nakiio";
+//document.getElementById("userrole").innerHTML = "Role : Admin";
